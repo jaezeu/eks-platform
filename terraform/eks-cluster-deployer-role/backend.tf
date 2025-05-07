915ce85781e00b5c077531e5e7c43d0dd8f98fdc
jaezeu <jazeel.meerasah@gmail.com>
2025-05-07 15:41:07 +0800

    Update backend config & tf provider version

--- terraform/eks-cluster-deployer-role/backend.tf
+++ terraform/eks-cluster-deployer-role/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "sctp-ce8-tfstate"
-    key    = "ce8-github-deployer-role.tfstate" #Update accordingly
+    bucket = "sctp-core-tfstate"
+    key    = "shared-eks-cluster-deployer-role.tfstate" #Update accordingly
     region = "ap-southeast-1"
   }
 }
