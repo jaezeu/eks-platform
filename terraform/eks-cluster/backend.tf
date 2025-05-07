f3d2c32c58a07d021153f5594c754517ffb3d442
jaezeu <jazeel.meerasah@gmail.com>
2025-05-07 16:26:45 +0800

    Add required version block

--- terraform/eks-cluster/backend.tf
+++ terraform/eks-cluster/backend.tf
@@ -1,7 +1,14 @@
 terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.97.0"
+    }
+  }
   backend "s3" {
     bucket = "sctp-core-tfstate"
-    key    = "shared-eks-cluster.tfstate" #Update accordingly
+    key    = "shared-eks-cluster.tfstate"
     region = "ap-southeast-1"
   }
 }
