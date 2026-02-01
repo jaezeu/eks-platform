4eedf766fa3a0b4174f54a99011ecde561808860
jaezeu <jazeel.meerasah@gmail.com>
2026-02-01 18:21:48 +0800

    Add tflint checks

--- terraform/eks-cluster/variables.tf
+++ terraform/eks-cluster/variables.tf
@@ -1,17 +1,20 @@
 # Set to true if you're making use of ExternalDNS with Route53
 variable "enable_external_dns" {
+  description = "Set to true to create IAM role and policies for ExternalDNS with Route53"
   type    = bool
   default = true
 }
 
 # Set to true if you're making use of Loki with a s3 backend
 variable "enable_loki_s3" {
+  description = "Set to true to create IAM role and policies for Loki with a s3 backend"
   type    = bool
   default = true
 }
 
 # Set to true if you're making use of a PersistentVolume with EBS CSI Driver Add-ons
 variable "enable_ebs_csi_driver_role" {
+  description = "Set to true to create IAM role and policies for PersistentVolume with EBS CSI Driver Add-ons"
   type    = bool
   default = true
 }
