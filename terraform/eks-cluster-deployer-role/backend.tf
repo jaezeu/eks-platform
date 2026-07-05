3bfbc59f0fa34cb2235bb91e13e15f5d6c407738
jaezeu <jazeel.meerasah@gmail.com>
2026-07-05 11:10:59 +0800

    Remove lockfile

--- terraform/eks-cluster-deployer-role/backend.tf
+++ terraform/eks-cluster-deployer-role/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket       = "sctp-core-tfstate"
     key          = "shared-eks-cluster-deployer-role.tfstate" #Update accordingly
     region       = "ap-southeast-1"
-    use_lockfile = true # S3-native state locking so concurrent workflow runs cannot corrupt state
   }
 }
