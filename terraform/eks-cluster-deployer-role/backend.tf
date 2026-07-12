5a09cc9c63ce9bea9b678a79f9cfa0ddc8543840
jaezeu <jazeel.meerasah@gmail.com>
2026-07-12 22:25:08 +0800

    chore: format terraform

--- terraform/eks-cluster-deployer-role/backend.tf
+++ terraform/eks-cluster-deployer-role/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "sctp-core-tfstate"
-    key          = "shared-eks-cluster-deployer-role.tfstate" #Update accordingly
-    region       = "ap-southeast-1"
+    bucket = "sctp-core-tfstate"
+    key    = "shared-eks-cluster-deployer-role.tfstate" #Update accordingly
+    region = "ap-southeast-1"
   }
 }
